f77f83325876d94daddae31fff03858fca06716a
loofmot <lshultzy@gmail.com>
2020-11-09 02:47:39 -0500

    New version of code that can pass the FreeCodeCamp test suite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
@@ -15,16 +16,16 @@ const beats = [
                {btn: 'C', name: 'Vocal-2', url: 'https://sampleswap.org/samples-ghost/DRUMS%20(FULL%20KITS)/STYLE%20KITS%20(Hip%20Hop,%20Etc)/Moombah%20Kit/63[kb]Moombah-Vocal-2.wav.mp3'}
               ];
 
-
 class DrumMachine extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      name: ''
+      objects: beats,
+      display: ''
     }
     this.onClick = this.onClick.bind(this);
     this.onKeyPress = this.onKeyPress.bind(this);
-    this.playSound = this.playSound.bind(this);
+
   }
 componentDidMount() {
     document.addEventListener('keypress', this.onKeyPress);
@@ -33,50 +34,49 @@ componentWillUnmount() {
     document.removeEventListener('keypress', this.onKeyPress);
   }
  onKeyPress(event){
-   var sound = beats.filter(x=> x.btn === event.key.toUpperCase());
-   if(sound[0]){   
-   this.setState ({
-      name: sound[0].name
-    });
-   document.getElementById(event.key.toUpperCase()).click();
-   }
+   var index = this.state.objects.findIndex(x=> x.btn === event.key.toUpperCase());
+   if(index>=0){
+    var audio = document.getElementById(event.key.toUpperCase());
+     this.setState({
+      display: this.state.objects[index].name
+      });
+     audio.play();
+   } 
  } 
   
  onClick(event){
-   var sound = beats.filter(x=> x.btn === event.target.id);
-    this.setState ({
-      name: sound[0].name
-    });
-   this.playSound(sound[0].url);
+  var audio = document.getElementById(event.target.value);
+   var ind = this.state.objects.findIndex(x=> x.btn === event.target.value);
+   this.setState({
+     display: this.state.objects[ind].name
+   });
+   audio.play();
  }
-  playSound(url){
-    var clip = new Audio(url);
-    clip.play();
-  }
-  
+
  render() {
   return(
+    
     <div class="brushed" id="drum-machine">
         <div class= "trans-bg panel" id="controls">
         <span class="oi text-LED" data-glyph="audio"></span> <h1 class="text-LED" id="title">{projTitle}</h1>
-        <div id="display">{this.state.name}</div>
+        <div id="display">{this.state.display}</div>
 
       </div>
       <div class="trans-bg panel" id="pads">
         <div class="row1 lines">
-          <button class="drum-pad" id="Q" onClick = {this.onClick}>Q</button>
-          <button class="drum-pad" id="W" onClick = {this.onClick}>W</button>   
-          <button class="drum-pad" id="E" onClick = {this.onClick}>E</button>  
+          <button class="drum-pad" value={this.state.objects[0].btn} id={this.state.objects[0].name} onClick = {this.onClick}>Q<audio class="clip" id="Q" src={this.state.objects[0].url}></audio></button>
+          <button class="drum-pad" value={this.state.objects[1].btn} id={this.state.objects[1].name} onClick = {this.onClick}>W<audio class="clip" id="W" src={this.state.objects[1].url}></audio></button>   
+          <button class="drum-pad" value={this.state.objects[2].btn} id={this.state.objects[2].name} onClick = {this.onClick}>E<audio class="clip" id="E" src={this.state.objects[2].url}></audio></button>  
         </div>
         <div class="row2 lines">
-          <button class="drum-pad" id="A" onClick = {this.onClick}>A</button>
-          <button class="drum-pad" id="S" onClick = {this.onClick}>S</button>   
-          <button class="drum-pad" id="D" onClick = {this.onClick}>D</button>   
+          <button class="drum-pad" value={this.state.objects[3].btn} id={this.state.objects[3].name} onClick = {this.onClick}>A<audio class="clip" id="A" src={this.state.objects[3].url}></audio></button>
+          <button class="drum-pad" value={this.state.objects[4].btn} id={this.state.objects[4].name} onClick = {this.onClick}>S<audio class="clip" id="S" src={this.state.objects[4].url}></audio></button>   
+          <button class="drum-pad" value={this.state.objects[5].btn} id={this.state.objects[5].name} onClick = {this.onClick}>D<audio class="clip" id="D" src={this.state.objects[5].url}></audio></button>   
         </div>
         <div class="row3 lines">
-          <button class="drum-pad" id="Z" onClick = {this.onClick}>Z</button>
-          <button class="drum-pad" id="X" onClick = {this.onClick}>X</button>   
-          <button class="drum-pad" id="C" onClick = {this.onClick}>C</button> 
+          <button class="drum-pad" value={this.state.objects[6].btn} id={this.state.objects[6].name} onClick = {this.onClick}>Z<audio class="clip" id="Z" src={this.state.objects[6].url}></audio></button>
+          <button class="drum-pad" value={this.state.objects[7].btn} id={this.state.objects[7].name} onClick = {this.onClick}>X<audio class="clip" id="X" src={this.state.objects[7].url}></audio></button>   
+          <button class="drum-pad" value={this.state.objects[8].btn} id={this.state.objects[8].name} onClick = {this.onClick}>C<audio class="clip" id="C" src={this.state.objects[8].url}></audio></button> 
         </div>
       </div>
     </div>
@@ -87,6 +87,7 @@ componentWillUnmount() {
   
   
 }
+
 
 ReactDOM.render(<DrumMachine />, document.getElementById('root'));
 
